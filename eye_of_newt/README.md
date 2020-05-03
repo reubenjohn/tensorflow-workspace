37f48a799f8513627c6d0a37ef215ad33fbefeb9
Reuben John <reubenvjohn@gmail.com>
2020-05-03 20:20:13 +0530

    Improved phase 2 deep architecture with skip connections.

--- eye_of_newt/README.md
+++ eye_of_newt/README.md
@@ -5,6 +5,9 @@ Eye of Newt
 Model the rich visual dynamics of diverse video sequences using unsupervised techniques to act as a powerful prior to be transferred to further experiments.  
 
 ### Roadmap
+
+---
+
 #### Phase 1. [Reproduce input image sanity check](experiments/image_reconstruction_sanity.py) 
 ![Status|Complete](https://img.shields.io/badge/Status-Complete-brightgreen)
    
@@ -14,40 +17,57 @@ This experiment is a sanity check and establishes a basic implementation style.
 |![Reconstruction Sanity](screenshots/reconstruction-sanity-outputs.png)|![Reconstruction Sanity Graph](screenshots/reconstruction-sanity-model.png)|
 |---|---|
 |Screenshots of input images and corresponding reconstructions at step 210(left), 300(middle) and 5640(right).|Simple model with essentially only 2 dense layers|
-**Training time** ~2min to ~0 loss
+|**Training time**|~2min to ~0 loss|
+
+---
 
 #### Phase 2. Deep image reproduction
 ![Early](https://img.shields.io/badge/Status-Final%20Stage-lightgreen)
 
 Same as Phase 1, but with deep neural network architectures.
 
-|Architecture|Output|Model|
+|Architecture|Model|Output|
 |---|---|---|
-|Shallow Conv-Deconv|![Shallow Model][1]|![Shallow Output][2]|
-|Deep Conv-Deconv|![Deep Model][3]|![Deep Output][4]|
-|-|Simple model with essentially only 2 dense layers|Screenshots of reconstructions at early(left), intermediate(middle) and late(right) stages of training|
+|**V1: Shallow Conv-Deconv**
+1-Conv-1-Deconv|![Shallow Model][1]|![Shallow Output][2]|
+|**V2: Deep Conv-Deconv**
+4-Conv-4-Deconv Cannot converge to 100% accuracy due to loss of fine grained spacial information in middle layers|![Deep Model][3]|![Deep Output][4]|
+|**V3: Deep With Skip Connections**
+Converges faster to 97% accuracy as compared to v2 by introducing skip connections from input to deep layers. However, obviously, non-skip connections eventually die as network learns to exploit skip connections!|![Deep Skip Model][5]|![Deep Output][6]|
+|**V4: Deep With Noisy Skip Connections (WIP)**
+Noise in skip connection forces network to learn high-dimensional embeddings of input, whilst leveraging fine grained details of input. Great stepping-stone to phase 4 where inputs don't equal targets|-|-|
 
 [1]: screenshots/reconstruction-image-sanity-shallow-conv-model.png
 [2]: screenshots/reconstruction-image-sanity-shallow-conv-output.png
 [3]: screenshots/reconstruction-image-sanity-deep-model.png
 [4]: screenshots/reconstruction-image-sanity-deep-output.png
+[5]: screenshots/reconstruction-image-sanity-deep-skip-model.png
+[6]: screenshots/reconstruction-image-sanity-deep-skip-output.png
 
-![Training Loss and MSE](screenshots/reconstruction-image-sanity-deep-training.png)  
-Blue: Shallow Architecture, Red: Deep Architecture
+##### Training
+![Training Loss and MSE](screenshots/phase2-training.png)
 
-**Training time** ~45min
+|Pink|Green|Gray|Training time|
+|---|---|---|---|
+|Shallow Architecture|Deep Architecture|Deep Skip Connection|~45min|
+
+---
 
 #### Phase 3. Generalized image reproduction  
 ![Early](https://img.shields.io/badge/Status-Pending-grey)
 
 Ability to reproduce input images outside the training data.
 
-#### Phase 3. Next frame prediction sanity check  
+---
+
+#### Phase 4. Next frame prediction sanity check  
 ![Early](https://img.shields.io/badge/Status-Pending-grey)
 
 Ability to predict the next frame of a video sequence from the training data.
 
-#### Phase 4. Generalized next frame prediction  
+---
+
+#### Phase 5. Generalized next frame prediction  
 ![Early](https://img.shields.io/badge/Status-Pending-grey)
   
 Ability to predict the next frame of a video sequence outside the training data.
--- eye_of_newt/experiments/deep_image_reconstruction_sanity.py
+++ eye_of_newt/experiments/deep_image_reconstruction_sanity.py
@@ -1,4 +1,5 @@
 import os
+from typing import Tuple
 
 import tensorflow as tf
 
@@ -9,8 +10,8 @@ from utils import configure_default_gpus, prefetch_to_available_gpu_device
 K = tf.keras
 
 IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS = 512, 352, 3
-BATCH_SIZE, N_SANITY_SAMPLES = 128, 3
-LOG_DIR = form_log_directory_path(experiment_name='deep-image-reconstruction-sanity/v3')
+BATCH_SIZE, N_SANITY_SAMPLES = 64, 3
+LOG_DIR = form_log_directory_path(experiment_name='deep-image-reconstruction-sanity/v4')
 
 configure_default_gpus()
 
@@ -21,22 +22,54 @@ dataset = tf.data.Dataset.list_files(DATASET_ROOT_DIR + '/TopGear/still_samples/
     .cache() \
     .repeat(128 * BATCH_SIZE) \
     .batch(BATCH_SIZE)
-dataset = prefetch_to_available_gpu_device(dataset, 2, use_workaround=True)
+dataset = prefetch_to_available_gpu_device(dataset, buffer_size=1, use_workaround=True)
+
 
 # Model
-model = K.models.Sequential([
-    K.layers.InputLayer(input_shape=(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS)),
-    K.layers.Conv2D(filters=8, kernel_size=(5, 5), strides=(2, 2), padding='same', activation=K.layers.LeakyReLU()),
-    K.layers.Conv2D(filters=16, kernel_size=(5, 5), strides=(2, 2), padding='same', activation=K.layers.LeakyReLU()),
-    K.layers.Conv2D(filters=32, kernel_size=(5, 5), strides=(2, 2), padding='same', activation=K.layers.LeakyReLU()),
-    K.layers.Conv2D(filters=64, kernel_size=(5, 5), strides=(2, 2), padding='same', activation=K.layers.LeakyReLU()),
-    K.layers.Conv2DTranspose(filters=32, kernel_size=(5, 5), strides=(2, 2), padding='same', activation=K.layers.LeakyReLU()),
-    K.layers.Conv2DTranspose(filters=16, kernel_size=(5, 5), strides=(2, 2), padding='same', activation=K.layers.LeakyReLU()),
-    K.layers.Conv2DTranspose(filters=8, kernel_size=(5, 5), strides=(2, 2), padding='same', activation=K.layers.LeakyReLU()),
-    K.layers.Conv2DTranspose(filters=3, kernel_size=(5, 5), strides=(2, 2), padding='same', activation=K.layers.LeakyReLU()),
-    K.layers.Reshape((IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS)),
-    K.layers.Lambda(lambda x: tf.clip_by_value(x / 2 + .5, 0., 1.), name='clip_color_channels')
-])
+def skip_connecting_auto_encoder_model(inputs,
+                                       symmetric_conv_configs: [dict],
+                                       middle_conv_configs: dict,
+                                       reconstruction_layer: K.layers.Layer):
+    activations = inputs
+    skip_connections = []
+    for conv_config in symmetric_conv_configs:
+        activations = K.layers.Conv2D(**conv_config)(activations)
+        skip_connections.append(activations)
+
+    activations = K.layers.Conv2D(**middle_conv_configs)(activations)
+    activations = K.layers.Conv2DTranspose(**middle_conv_configs)(activations)
+
+    for deconv_config, skip_connection in zip(reversed(symmetric_conv_configs), reversed(skip_connections)):
+        concatenated_activation = K.layers.Concatenate()([activations, skip_connection])
+        skip_connecting_deconv = K.layers.Conv2DTranspose(**deconv_config)
+        activations = skip_connecting_deconv(concatenated_activation)
+
+    activations = reconstruction_layer(activations)
+    activations = K.layers.Reshape((IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))(activations)
+    activations = NormalizeColorChannels()(activations)
+    return K.Model(inputs=inputs, outputs=activations)
+
+
+class NormalizeColorChannels(K.layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, inputs, **kwargs):
+        return tf.clip_by_value(inputs / 2 + .5, 0., 1.)
+
+
+model = skip_connecting_auto_encoder_model(
+    inputs=K.Input(shape=(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS)),
+    symmetric_conv_configs=[
+        dict(filters=8, kernel_size=(5, 5), strides=(2, 2), padding='same', activation=K.layers.LeakyReLU()),
+        dict(filters=16, kernel_size=(5, 5), strides=(2, 2), padding='same', activation=K.layers.LeakyReLU()),
+        dict(filters=32, kernel_size=(5, 5), strides=(2, 2), padding='same', activation=K.layers.LeakyReLU()),
+    ],
+    middle_conv_configs=dict(filters=64, kernel_size=(5, 5), strides=(2, 2), padding='same',
+                             activation=K.layers.LeakyReLU()),
+    reconstruction_layer=K.layers.Conv2D(filters=3, kernel_size=(3, 3), padding='same', activation=K.layers.LeakyReLU(),
+                                         name='reconstruct_color_channels')
+)
 
 # Training
 
@@ -64,7 +97,8 @@ def main():
     tb_callback = K.callbacks.TensorBoard(LOG_DIR)
     tb_callback.set_model(model)
     os.makedirs(LOG_DIR, exist_ok=True)
-    K.utils.plot_model(model, LOG_DIR + '/model.png', show_shapes=True, expand_nested=True)
+    K.utils.plot_model(model, LOG_DIR + '/reconstruction-image-sanity-deep-model.png', show_shapes=True,
+                       expand_nested=True)
     with tf.summary.create_file_writer(LOG_DIR + '/train').as_default() as writer:
         prev_steps = 0
         for epoch in range(3):
